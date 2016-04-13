978ffa42d1479d6d08c098c00a113296822f22ae
Frederick Silva <fredericksilva@outlook.com>
2016-04-13 17:43:46 +0000

    add news escolas ocupadas

--- js/app.js
+++ js/app.js
@@ -30,7 +30,212 @@ var app = angular.module('demo', []).controller('DemoCtrl', function($scope) {
             desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
             src:  '#OcupaEuclydes',
             home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaNery',
+            desc: 'Avenida Santa Cruz, 2 - Humberto Antunes, Mendes | C.E. Doutor João Nery',
+            src:  '#OcupaNery',
+            home: 'http://bit.ly/1q7pZ9X'
+        },
+        {
+            name: '#OcupaMatias',
+            desc: ' Rua Conde de Araruama, 439 - Centro, Macaé | C.E Matias Neto',
+            src:  '#OcupaMatias',
+            home: 'http://bit.ly/1UJhlvm'
+        },
+        {
+            name: '#OcupaBacaxá',
+            desc: ' Rua Capitão Nunes - Bacaxá, Saquarema | Escola Técnica Estadual Helber Vignoli Muniz',
+            src:  '#OcupaBacaxá',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
         }
+        
     ];
 
 });
