3748590bc63a1096ca3951d3a3e2c730b075dfb5
Frederick Silva <fredericksilva@outlook.com>
2016-04-13 17:37:22 +0000

    test twitter

--- js/app.js
+++ js/app.js
@@ -2,27 +2,34 @@
 var app = angular.module('demo', []).controller('DemoCtrl', function($scope) {
     $scope.escolas = [
         {
-            name: 'OcupaCairu',
-            desc: '#OcupaCairu',
-            src: 'https://www.facebook.com/OcupaCairu/',
-            home: 'https://www.facebook.com/OcupaCairu/'
-        },
-        {
-            name: '#OCUPA-JBM',
-            desc: '#OCUPA-JBM',
-            src: 'https://www.facebook.com/OCUPA-JBM-210806322632120',
-            home: 'https://www.facebook.com/OCUPA-JBM-210806322632120'
-        },
-        {
             name: '#OcupaMendes',
             desc: 'Rua Pio Dutra, 353 - Freguesia, Ilha do Governador | C.E. Prefeito Mendes de Morae',
-            src: 'http://bit.ly/1Vaa1IX',
+            src:  '#OcupaMendes',
             home: 'http://bit.ly/1Vaa1IX'
-        },{
+        },
+        {
             name: '#OcupaGomes',
             desc: 'Rua São Maurício, 87 - Penha, Rio de Janeiro | C.E. Gomes Freire de Andra',
-            src: 'http://bit.ly/1UJduOW',
+            src:  '#OcupaGomes',
             home: 'http://bit.ly/1UJduOW'
+        },
+        {
+            name: '#OcupaHL',
+            desc: 'Rua Cuba, 320 - Penha, Rio de Janeiro | C.E. Heitor Lira',
+            src:  '#OcupaHL',
+            home: 'http://bit.ly/1UJduOW'
+        },
+        {
+            name: '#OcupaCairu',
+            desc: 'Rua Soares, 95 - Méier, Rio de Janeiro | C.E. Visconde de Cairú',
+            src:  '#OcupaCairu',
+            home: 'http://bit.ly/1UJduOW'
+        },
+        {
+            name: '#OcupaEuclydes',
+            desc: 'Rodovia Amaral Peixoto, Lote 1 - São José de Imbassaí, Maricá | C.E. Euclydes Paulo da Silva',
+            src:  '#OcupaEuclydes',
+            home: 'http://bit.ly/1q7pA7r'
         }
     ];
 
